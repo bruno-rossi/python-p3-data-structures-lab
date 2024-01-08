b96a60a51ffc8b61e9b2527727609cd96f5c3b72
Bruno Rossi <brunogabrielrossi@gmail.com>
2024-01-08 17:05:03 -0500

    Data structures lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,59 @@ spicy_foods = [
     },
 ]
 
+# Practice using loops and Python list comprehensions alongside list and dict methods to solve these deliverables.
+
+# Define a function get_names() that takes a list of spicy_foods and returns a list of strings with the names of each spicy food.
+
 def get_names(spicy_foods):
-    pass
+    # new_list = [optional_operation(item) for item in old_list if optional_condition == True]
+    return [food["name"] for food in spicy_foods]
+
+# get_names(spicy_foods)
+
+# Define a function get_spiciest_foods() that takes a list of spicy_foods and returns a list of dictionaries where the heat level of the food is greater than 5.
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
+
+# Define a function print_spicy_foods() that takes a list of spicy_foods and output to the terminal each spicy food in the following format using print(): Buffalo Wings (American) | Heat Level: 🌶🌶🌶.
 
 def print_spicy_foods(spicy_foods):
-    pass
+    [print(f"{food['name']} ({food['cuisine']}) | Heat Level: {food['heat_level'] * '🌶'}") for food in spicy_foods]
+
+# print_spicy_foods(spicy_foods)
+
+# Define a function get_spicy_food_by_cuisine() that takes a list of spicy_foods and a string representing a cuisine, and returns a single dictionary for the spicy food whose cuisine matches the cuisine being passed to the method.
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+    return None
+
+# get_spicy_food_by_cuisine(spicy_foods, "American")
+
+# Define a function print_spiciest_foods() that takes a list of spicy_foods and outputs to the terminal ONLY the spicy foods that have a heat level greater than 5, in the following format:
+
+# Buffalo Wings (American) | Heat Level: 🌶🌶🌶.
+
+# Try to use functions you've already written to solve this!
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: {food['heat_level'] * '🌶'}")
+
+# Define a function average_heat_level() that takes a list of spicy_foods and returns an integer representing the average heat level of all the spicy foods in the array. Recall that to derive the average of a collection, you need to calculate the total and divide number of elements in the collection.
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat_levels = [food["heat_level"] for food in spicy_foods]
+    return sum(heat_levels) / len(heat_levels)
+
+# get_average_heat_level(spicy_foods)
+
+# Define a function create_spicy_food() that takes a list of spicy_foods and a new spicy_food and returns the original list with the new spicy_food added.
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
